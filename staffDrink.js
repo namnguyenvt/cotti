d3a47c32c7a4990e0ac5ca41a850e5ccf2e72720
Nam <nguyennam11022005@gmail.com>
2024-11-20 23:43:47 +1100

    Finished Searchable List

--- staffDrink.js
+++ staffDrink.js
@@ -136,3 +136,22 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+function searchableList() {
+    var input, filter;
+    input = document.getElementById('searchBox');
+    filter = input.value.toUpperCase();
+    var ul = document.getElementById("drinkList");
+    var li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (let i = 0; i < li.length; i++) {
+        var a = li[i].getElementsByTagName("a")[0];
+        var txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
