364089094bd5a7ac0299f6a659a5757fa307463a
Nam <nguyennam11022005@gmail.com>
2024-11-22 16:18:40 +1100

    Show content when hit button

--- staffDrink.js
+++ staffDrink.js
@@ -155,7 +155,7 @@ function searchableList() {
 
     // Loop through all list items, and hide those who don't match the search query
     for (let i = 0; i < li.length; i++) {
-        var a = li[i].getElementsByTagName("a")[0];
+        var a = li[i].getElementsByTagName("button")[0];
         var txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
